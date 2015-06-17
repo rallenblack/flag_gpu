aaab4ed1e3257f47d41944175297d84a17e05609
Richard Black <rallenblack@gmail.com>
2015-06-17 09:39:26 -0600

    Added comments to packet_gen.m

--- lib/xGPU/src/xgpu_version.h
+++ lib/xGPU/src/xgpu_version.h
@@ -1,1 +1,1 @@
-#define XGPU_VERSION 1.5.0+23@gde8253f-dirty
+#define XGPU_VERSION +dirty
--- utils/pkt_gen/packet_gen.m
+++ utils/pkt_gen/packet_gen.m
@@ -1,26 +1,30 @@
 % FLAG packet generator
+clearvars;
+close all;
+
+% Clear any sockets opened by MATLAB
 u = instrfindall;
 delete(u);
 
 % System Constants
-fs        = 155e6;
-Ninputs   = 40;
-Nbins     = 500;
-Nfft      = 512;
-Nfengines = 5;
-Nxengines = 10;
+fs        = 155e6; % Sampling frequency - used for noise level
+Ninputs   = 40;    % Number of inputs/antennas
+Nbins     = 500;   % Total number of frequency bins
+Nfft      = 512;   % F-engine FFT size
+Nfengines = 5;     % Number of F-engines
+Nxengines = 10;    % Number of X-engines (i.e. Number of GPUs)
 
-Nin_per_f        = Ninputs/Nfengines;
-Nbin_per_x       = Nbins/Nxengines;
-Ntime_per_packet = 10;
+Nin_per_f        = Ninputs/Nfengines; % Number of inputs per F-engine
+Nbin_per_x       = Nbins/Nxengines; % Number of bins per X-engine
+Ntime_per_packet = 10; % Number of time samples (spectra snapshots) per packet
 
-quant_res = 0.5e-8;
+quant_res = 0.5e-8; % Arbitrary quantization resolution for noise generation
 
-% Noise parameters
-kb     = 1.3806488e-23;
-Tsys   = 45;
-BW     = fs/Nfft;
-sigma2 = kb*Tsys*BW;
+% Noise parameters - only used if data_flag = 1
+kb     = 1.3806488e-23; % Boltzmann's constant
+Tsys   = 45;            % System temperature
+BW     = fs/Nfft;       % Channel bandwidth
+sigma2 = kb*Tsys*BW;    % Noise power per channel
 
 
 % Flag to control what is sent
@@ -35,19 +39,18 @@ data_flag = 3;
 % It should be noted that the phase of the sinusoid will not change between
 % time samples-- this is just for convenience. A more sophisticated packet
 % generator would incorporate the phase shifts across time.
-s_bin = 1; % Sinusoid's bin number (1-500)
-s_ele = 1; % Sinusoid's element number (1-40)
+s_bin = 1; % Sinusoid's absolute bin number (1-500)
+s_ele = 1; % Sinusoid's absolute element number (1-40)
 s_phi = pi/2; % Sinusoid's phase (magnitude is set to 1)
-s_xid = floor((s_bin - 1)/100) + 1;
-s_fid = floor((s_ele - 1)/8) + 1;
+s_xid = floor((s_bin - 1)/100) + 1; % X-engine ID for desired bin
+s_fid = floor((s_ele - 1)/8) + 1;   % F-engine ID for desired input
 s_bin_r = mod(s_bin - 1, 100) + 1; % Relative bin number (internal fengine index)
 s_ele_r = mod(s_bin - 1, 8) + 1; % Relative element number
 
 
-
-% Create UDP sockets
+% Create UDP sockets - 1 IP address per Xengine (xid)
 for xid = 1:Nxengines
-    remoteHost = ['10.10.', num2str(xid+3), '.233'];
+    remoteHost = ['10.10.', num2str(xid), '.233'];
     sock(xid) = udp(remoteHost, 'RemotePort', 8511, 'LocalPort', 8511);
     set(sock(xid), 'OutputBufferSize', 9000);
     set(sock(xid), 'OutputDatagramPacketSize', 9000);
@@ -55,20 +58,30 @@ end
 
 
 % Generate packet payloads
-mcnt = 0;
+mcnt = 0; % Each mcnt represents 10 packets across all F-engines in the
+          % same time frame
 while mcnt <= 1000
     disp(['Sending mcnt = ', num2str(mcnt)]);
-    for xid = 1:1
+    for xid = 1:1 % Set to a single X-engine for single HPC testing (Richard B.)
         for fid = 1:Nfengines
             w_idx = 1;
             
             % Create packet header
+            % header contains the following
+            % MSB = 1
+            % bits  1-44: mcnt
+            % bits 45-49: cal
+            % bits 49-56: F-engine ID (fid)
+            % bits 57-64: X-engine ID
+            % LSB = 64
             header = uint64(mcnt)*2^20 + uint64(15)*2^16 + uint64(fid-1)*2^8 + uint64(xid-1);
             
             % Allocate memory for packet payload
             payload = zeros(16*Ntime_per_packet*Nbin_per_x+8, 1, 'uint8');
             
             % Shift header information into packet
+            % (The socket wants bytes, so we need to mask and shift to get
+            % uint8)
             payload(w_idx) = uint8(bitand(bitshift(header, -56), (2^8 - 1)));
             w_idx = w_idx + 1;
             payload(w_idx) = uint8(bitand(bitshift(header, -48), (2^8 - 1)));
